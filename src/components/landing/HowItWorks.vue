a8351ca738edb70f7446066c728f589c27f89b7c
wkulikowski <wojciech.kulikowski@10clouds.com>
2020-03-21 00:06:11 +0100

    Change how-it-works screen

--- src/components/landing/HowItWorks.vue
+++ src/components/landing/HowItWorks.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="how-it-works">
+  <div class="how-it-works flex row">
+    <div class="how-it-works__text container">
+      <h1 class="heading--hero">How it works</h1>
+      <h2 class="copy--large">We work together with the local businesses in Poland to help them generate an income and ensure they stay afloat in the times of trouble</h2>
+      <div class="how-it-works__list flex column">
+        <div class="how-it-works__item flex column">
+          <div class="flex row"> 
+            <div class="how-it-works__number"><span>1</span></div>
+            <h3 class="heading heading--primary heading">Find a business</h3>
+          </div>
+          <p class="copy copy">Start by finding the local business you wish to support. View their page to see all the available services for purchase. </p>
+        </div>
+        <div class="how-it-works__item flex column">
+          <div class="flex row"> 
+            <div class="how-it-works__number"><span>2</span></div>
+            <h3 class="heading heading--primary heading">Purchase a service</h3>
+          </div>
+          <p class="copy copy">By purchasing one of the service, you’ll be supporting the business financially in these troubled times.</p>
+        </div>
+        <div class="how-it-works__item flex column">
+          <div class="flex row"> 
+            <div class="how-it-works__number"><span>3</span></div>
+            <h3 class="heading heading--primary heading">Use it in the future</h3>
+          </div>
+          <p class="copy copy">Once the Polish government gives the okay for businesses to start operating again, you can cash in this purchase</p>
+        </div>
+      </div>
+    </div>
     <div class="container">
-      <div class="how-it-works__list flex">
-        <div class="how-it-works__item flex column">
-          <div class="how-it-works__number"><span>1</span></div>
-          <h3 class="heading heading--primary heading--white">Pay now for a voucher</h3>
-          <p class="copy copy--white">Choose a company that is of interest to you and buy a voucher for goods and services</p>
-        </div>
-        <div class="how-it-works__item flex column">
-          <div class="how-it-works__number"><span>2</span></div>
-          <h3 class="heading heading--primary heading--white">The company has income to survive</h3>
-          <p class="copy copy--white">Companies around the world will struggle with finances during the coronavirus crisis. We need to make sure people don't lose jobs and the economy doesn't shrink.</p>
-        </div>
-        <div class="how-it-works__item flex column">
-          <div class="how-it-works__number"><span>3</span></div>
-          <h3 class="heading heading--primary heading--white">Use it in the future</h3>
-          <p class="copy copy--white">When COVID-19 pandemic is finally over, you will be able to pay for goods and services with your voucher</p>
-        </div>
+      <div class="illustration_span">
+        <div class="illustration-background"></div>
+        <img class="illustration_image" src="../../assets/how-it-works-illu.svg" alt="empty restaurant">
       </div>
     </div>
   </div>
@@ -30,34 +44,62 @@ export default {
 
 <style scoped lang="scss">
   .how-it-works {
-    background: color(primary);
-    padding: 100px 0;
+    background: color(pink-background);
+    padding: 150px 0px;
+    padding-left: 450px;
 
-    &__list {
 
+    &__text {
+      width: 500px;
+      margin-left: auto;
     }
 
     &__item {
-      padding: 0 50px;
-      width: 33%;
+      width: 80%;
 
       h3 {
         max-width: 80%;
       }
+
+      .copy {
+        padding-left: 50px;
+      }
     }
 
     &__number {
-      background: color(primary-x-light);
+      background: color(pink);
       position: relative;
-      height: 100px;
-      width: 100px;
+      margin-right: 15px;
+      margin-top: 7px;
+      height: 36px;
+      width: 36px;
       border-radius: 50%;
 
       span {
         @include center();
-        color: white;
-        @include font(60, 600);
+        color: color(text);
+        @include font(18, 600);
       }
     }
+  }
+
+  .illustration_span {
+    position: relative;
+    padding-left: 100px;
+  }
+
+  .illustration_image {
+    position: absolute;
+    width: 366px;
+    height: 488px;
+  }
+
+  .illustration-background {
+    position: absolute;
+    left: 150px;
+    bottom: -550px;
+    width: 366px;
+    height: 515px;
+    background-color: color(text-pink);
   }
 </style>
