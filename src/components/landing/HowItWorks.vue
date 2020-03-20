ba6d023c8da5d1b3deb3b7122c66c9b89fa3424f
BartoszBilejczyk <bartosz.bilejczyk@10clouds.com>
2020-03-21 00:34:03 +0100

    fix

--- src/components/landing/HowItWorks.vue
+++ src/components/landing/HowItWorks.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="how-it-works flex row">
-    <div class="how-it-works__text container">
-      <h1 class="heading--hero">How it works</h1>
-      <h2 class="copy--large">We work together with the local businesses in Poland to help them generate an income and ensure they stay afloat in the times of trouble</h2>
-      <div class="how-it-works__list flex column">
-        <div class="how-it-works__item flex column">
-          <div class="flex row"> 
-            <div class="how-it-works__number"><span>1</span></div>
-            <h3 class="heading heading--primary heading">Find a business</h3>
+  <div class="how-it-works">
+    <div class="container flex">
+      <div class="how-it-works__text">
+        <h1 class="heading heading--hero">How it works</h1>
+        <h2 class="copy copy--large">We work together with the local businesses in Poland to help them generate an income and ensure they stay afloat in the times of trouble</h2>
+        <div class="how-it-works__list flex column">
+          <div class="how-it-works__item flex column">
+            <div class="flex">
+              <div class="how-it-works__number"><span>1</span></div>
+              <h3 class="heading heading--primary heading">Find a business</h3>
+            </div>
+            <p class="copy copy">Start by finding the local business you wish to support. View their page to see all the available services for purchase. </p>
           </div>
-          <p class="copy copy">Start by finding the local business you wish to support. View their page to see all the available services for purchase. </p>
-        </div>
-        <div class="how-it-works__item flex column">
-          <div class="flex row"> 
-            <div class="how-it-works__number"><span>2</span></div>
-            <h3 class="heading heading--primary heading">Purchase a service</h3>
+          <div class="how-it-works__item flex column">
+            <div class="flex">
+              <div class="how-it-works__number"><span>2</span></div>
+              <h3 class="heading heading--primary heading">Purchase a service</h3>
+            </div>
+            <p class="copy copy">By purchasing one of the service, you’ll be supporting the business financially in these troubled times.</p>
           </div>
-          <p class="copy copy">By purchasing one of the service, you’ll be supporting the business financially in these troubled times.</p>
-        </div>
-        <div class="how-it-works__item flex column">
-          <div class="flex row"> 
-            <div class="how-it-works__number"><span>3</span></div>
-            <h3 class="heading heading--primary heading">Use it in the future</h3>
+          <div class="how-it-works__item flex column">
+            <div class="flex">
+              <div class="how-it-works__number"><span>3</span></div>
+              <h3 class="heading heading--primary heading">Use it in the future</h3>
+            </div>
+            <p class="copy copy">Once the Polish government gives the okay for businesses to start operating again, you can cash in this purchase</p>
           </div>
-          <p class="copy copy">Once the Polish government gives the okay for businesses to start operating again, you can cash in this purchase</p>
         </div>
       </div>
-    </div>
-    <div class="container">
       <div class="illustration_span">
         <div class="illustration-background"></div>
         <img class="illustration_image" src="../../assets/how-it-works-illu.svg" alt="empty restaurant">
@@ -37,32 +37,42 @@
 </template>
 
 <script>
-export default {
-  name: 'how-it-works',
-}
+  export default {
+    name: 'HowItWorks',
+  }
 </script>
 
 <style scoped lang="scss">
   .how-it-works {
     background: color(pink-background);
     padding: 150px 0px;
-    padding-left: 450px;
-
 
     &__text {
-      width: 500px;
-      margin-left: auto;
+      width: 50%;
+      padding-left: 150px;
+
+      > h2 {
+        max-width: 500px;
+        line-height: 40px;
+      }
+    }
+
+    &__list {
+      margin-top: 40px;
     }
 
     &__item {
       width: 80%;
+      margin-bottom: 10px;
 
       h3 {
         max-width: 80%;
+        margin-bottom: 0;
       }
 
       .copy {
         padding-left: 50px;
+        margin-top: 0;
       }
     }
 
@@ -85,21 +95,22 @@ export default {
 
   .illustration_span {
     position: relative;
-    padding-left: 100px;
+    margin-left: 100px;
   }
 
   .illustration_image {
-    position: absolute;
     width: 366px;
-    height: 488px;
+    height: 515px;
+    z-index: 10;
+    position: relative;
   }
 
   .illustration-background {
     position: absolute;
-    left: 150px;
-    bottom: -550px;
+    left: 80px;
+    top: 80px;
     width: 366px;
-    height: 515px;
+    height: 545px;
     background-color: color(text-pink);
   }
 </style>
