fc380216753038a80973067ba9e39e81c4d00cdb
wkulikowski <wojciech.kulikowski@10clouds.com>
2020-03-21 16:15:02 +0100

    Debug getters

--- src/components/company/CompanyInfo.vue
+++ src/components/company/CompanyInfo.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="company-info">
-
+    <Business></Business>
   </div>
 </template>
-
 <script>
+
+// :business="business"
+import Business from '@/components/common/Business.vue'
+
 export default {
   name: 'CompanyInfo',
+  components: {
+    Business
+  },
+  computed: {
+    getOneBusiness() {
+      return this.$store.state.getOneBusiness;
+    }
+  }
   // WOJTEK https://vuex.vuejs.org/guide/getters.html use computed here to use getter from store and access the getOneBusiness getter.
   // Then use it in template in Business component. You can find how to import a component in almost every other view.
-  // On this view, it should be possible to already click when you implement 
+  // On this view, it should be possible to already click when you implement
 }
 </script>
 
