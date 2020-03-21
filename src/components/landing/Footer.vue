e3dce09da6210616ff29240eaeabb2793face2f8
wkulikowski <wojciech.kulikowski@10clouds.com>
2020-03-21 12:51:08 +0100

    CR improvements

--- src/components/landing/Footer.vue
+++ src/components/landing/Footer.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="footer-box">
+  <div class="footer-box text-center">
     <div class="flex column">
       <h1 class="heading--hero">Find the local business you wish to support</h1>
       <div class="footer-box__search hero-input flex column align-center justify-center">
-        <div class="input-wrapper">
+        <div class="input-wrapper input--alone">
           <input placeholder="Search for a local business..." class="input" type="text">
           <img src="../../assets/icon-search.svg">
         </div>
       </div>
-      <div class="footer-box__logo flex row">
+      <div class="footer-box__logo flex row justify-center">
         <img src="../../assets/logo.svg" alt=""/>
         eRelief
       </div>
@@ -27,11 +27,6 @@
     height: 600px;
     padding: 100px 350px;
     background-color: color(pink-light);
-    text-align: center;
-
-    &__logo {
-      justify-content: center;
-    }
 
     &__search {
       padding: 64px;
